b62b901ea9c7d978c19a012d9823dadeb07d66c7
EddyBautista <bautista.j.eddy@gmail.com>
2021-10-31 08:22:42 -0500

    Happy Halloween!

--- index.js
+++ index.js
@@ -5,8 +5,49 @@ const fetchShelves = async () =>{
     try {
         const response = await axios.get('https://www.amazon.com/s?crid=36QNR0DBY6M7J&k=shelves&ref=glow_cls&refresh=1&sprefix=s%2Caps%2C309');
         const html = response.data;
+        console.log(html);
+        const $ = cheerio.load(html);
+        const shelves = [];
+        // use cheerio to iterate over each product on the page
+        $('div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20').each((_idx,
+            el) => {
+                const shelf = $(el);
+                const title = shelf.find('span.a-size-base-plus.a-color-base.a-text-normal').text();
+                const image = shelf.find('img.s-image').attr('src')
+
+const link = shelf.find('a.a-link-normal.a-text-normal').attr('href')
+
+const reviews = shelf.find('div.a-section.a-spacing-none.a-spacing-top-micro > div.a-row.a-size-small').children('span').last().attr('aria-label')
+
+const stars = shelf.find('div.a-section.a-spacing-none.a-spacing-top-micro > div > span').attr('aria-label')
+
+const price = shelf.find('span.a-price > span.a-offscreen').text()
+
+
+    let element = {
+        title,
+        image,
+        link: `https://amazon.com${link}`,
+        price,
+    }
+
+    if (reviews) {
+        element.reviews = reviews
+    }
+
+    if (stars) {
+        element.stars = stars
+    }
+
+
+
+                shelves.push(title);
+            });
+            return shelves;
 
     }catch(error){
         throw error;
     }
-} 
+} ;
+
+fetchShelves();
